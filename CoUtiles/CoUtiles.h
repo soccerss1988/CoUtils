4f8289b2b4d41b3826dac7bdd5598203bd97a15b
Jason.Huang <soccerss1988@gmail.com>
2019-07-19 21:55:56 +0800

    init

---
+++ CoUtiles/CoUtiles.h
@@ -0,0 +1,19 @@
+//
+//  CoUtiles.h
+//  CoUtiles
+//
+//  Created by YJ Huang on 2019/7/19.
+//  Copyright © 2019 YJ Huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoUtiles.
+FOUNDATION_EXPORT double CoUtilesVersionNumber;
+
+//! Project version string for CoUtiles.
+FOUNDATION_EXPORT const unsigned char CoUtilesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoUtiles/PublicHeader.h>
+
+
